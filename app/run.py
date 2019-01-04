24d329c6519e1ff5d61a8f03bfa69b18c045af60
Sylvain <bond.never.die@gmail.com>
2019-01-04 17:33:46 +0100

    add token authentication to API

--- app/run.py
+++ app/run.py
@@ -1,6 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from eve import Eve
-app = Eve()
+from eve.auth import TokenAuth
+from flask import current_app as app
+
+class TokenAuth(TokenAuth):
+    def check_auth(self, token, allowed_roles, resource, method):
+        users = app.data.driver.db['users']
+        return users.find_one({'token': token})
 
 if __name__ == '__main__':
+    app = Eve(auth=TokenAuth)
     app.run()
 
