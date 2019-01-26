e0abdcbac4de598b44497e25899e7d270a11c9e3
Sylvain <bond.never.die@gmail.com>
2019-01-26 20:44:48 +0100

    Access-Control-Expose-Headers

--- app/settings.py
+++ app/settings.py
@@ -93,7 +93,8 @@ rendezvous = {
     'schema': rendezvous_schema,
 }
 
-X_HEADERS = ['Authorization','Content-type', 'If-Match']
+X_HEADERS = ['Authorization','Content-type','If-Match']
+X_EXPOSE_HEADERS = ['Access-Control-*']
 X_DOMAINS = os.environ.get("X_DOMAINS")
 MONGO_URI = os.environ.get("MONGO_URI")
 DOMAIN = {'users': users, 'locations': locations, 'rendezvous': rendezvous}
